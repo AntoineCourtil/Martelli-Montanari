c9c7bb22d4fc9d7a44d8708c3c27c3f74e15c61b
Simon HAJEK <simon@sup.sarl>
2017-12-04 14:28:32 +0100

    INIT

---
+++ Martelli_Montanari.pl
@@ -0,0 +1,7 @@
+run :-
+    write('Programme réalisé par Antoine Courtil et Simon Hajek'),
+    write('\nAlgorithme d’unification de Martelli-Montanari vu avec M. Galmiche'),
+    begin.
+    
+begin :-
+    write('\n\nPour lancer ...').
